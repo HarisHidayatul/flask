590eaf2f8e84d3148725b899a45a6518bd4d1a3e
HarisHidayatul <Hidayatulloh.haris@gmail.com>
2023-04-07 21:26:59 +0700

    Mengubah app

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return "Hello world"
+    return jsonify({'status' : 'running'})
+
+@app.route("/order", methods=["POST"])
+def create_order():
+    data = request.get_json()
+    print('Request Data: ' + str(data))
+    return jsonify({'msg' : 'Order Created Sucessfully'})
 
 if __name__ == "main":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
